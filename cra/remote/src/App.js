3f378a4e240ef83ffada157215f196ee45f64ee0
BKutlar <brayankutlar94@gmail.com>
2024-01-19 16:43:18 +0100

    NavBar file

--- cra/remote/src/App.js
+++ cra/remote/src/App.js
@@ -3,15 +3,11 @@ import Header from './components/Header';
 import HeroSection from './components/HeroSection';
 import Features from './components/Features';
 import ContactForm from './components/ContactForm';
+import NavBar from './components/NavBar';
 
 const App = () => (
   <div>
-    <div id='nav' style={{ zIndex: '1', marginBottom: '16px' }}>
-      <nav style={{ background: '#0d6efd', width: '100%', height: 'auto', position: 'fixed', top: '0', left: '0' }}>
-        landing page
-      </nav>
-    </div>
-
+    <NavBar />
     <h1>Basic Host-Remote</h1>
     <h2>Remote</h2>
     <LocalButton />
