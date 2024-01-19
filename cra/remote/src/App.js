ba32cc6856a14241bc909d0bba93ed644f74fc92
BKutlar <brayankutlar94@gmail.com>
2024-01-19 15:13:47 +0100

    add imports

--- cra/remote/src/App.js
+++ cra/remote/src/App.js
@@ -1,9 +1,13 @@
 import LocalButton from './Button';
+import Header from './components/Header';
+import HeroSection from './components/HeroSection';
+import Features from './components/Features';
+import ContactForm from './components/ContactForm';
 
 const App = () => (
   <div>
     <div id='nav' style={{ zIndex: '1', marginBottom: '16px' }}>
-      <nav style={{ background: '#0d6efd', width: '100%', height: 'auto', position: 'fixed', top: '0', left: '0'}}>
+      <nav style={{ background: '#0d6efd', width: '100%', height: 'auto', position: 'fixed', top: '0', left: '0' }}>
         landing page
       </nav>
     </div>
@@ -11,10 +15,10 @@ const App = () => (
     <h1>Basic Host-Remote</h1>
     <h2>Remote</h2>
     <LocalButton />
-      <Header />
-      <HeroSection />
-      <Features />
-      <ContactForm />
+    <Header />
+    <HeroSection />
+    <Features />
+    <ContactForm />
   </div>
 );
 
